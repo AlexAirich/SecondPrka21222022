ca10e3b712255eaa7358361bae1ccec4ccd5e674
K0MED1ANT <69312748+K0MED1ANT@users.noreply.github.com>
2022-12-21 17:36:56 +0600

    first commit

--- src/App.vue
+++ src/App.vue
@@ -9,31 +9,27 @@
             @remove="removeEmployee"
         ></EmployeeList>
 
-        <EmployeeResult
-        :employees="employees"
-            @price="totalPrice"
-        ></EmployeeResult>
+    <p>Итого без налоговых вычетов: {{ totalWithoutTax }}₸</p>
+    <p>Итого с налоговыми вычетами: {{ totalWithTax }}₸</p>
   </div>
 </template>
 <script>
 
 import EmployeeForm from "@/components/EmployeeForm.vue";
 import EmployeeList from "@/components/EmployeeList.vue";
-import EmployeeResult from "@/components/EmployeeResult.vue";
 
 export default {
 
     components: {
-        EmployeeForm, EmployeeList, EmployeeResult
+        EmployeeForm, EmployeeList
     },
 
     data() {
         return {
-            employees: [
-                {fullName: "Калугина Дарина Марковна", date: "17-10-2022", daysWork: 12, salary: 1200},
-                {fullName: "Богданова Юлия Артёмовна", date: "01-10-2022", daysWork: 23, salary: 2500},
-                {fullName: "Фролова Дарья Александровна", date: "22-09-2022", daysWork: 16, salary: 1700},
-            ],
+            employees: [                
+                {fullName: "Айрих Алексей Алексеевич", date: "21-11-2022", daysWork: 16, salary: 200000},
+                {fullName: "Капирбек Альмансур Бексултанович", date: "15-11-2022", daysWork: 54, salary: 150000},
+                {fullName: "Сафонов Сергей Иванович", date: "24-11-2022", daysWork: 19, salary: 300000},],
         }
     },
     methods: {
@@ -96,6 +92,14 @@ export default {
         }
         
     },
+            computed: {
+    totalWithoutTax() {
+      return this.employees.reduce((acc, employees) => acc + employees.salary, 0)
+    },
+    totalWithTax() {
+      return this.employees.reduce((acc, employees) => acc + employees.salary * 0.85, 0)
+    }
+  }
 }
 </script>
 <style>
