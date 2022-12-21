28dbdbf8ab70040fb4174ca20f5c5831c9987eb1
K0MED1ANT <69312748+K0MED1ANT@users.noreply.github.com>
2022-12-21 16:44:52 +0600

    first commit

---
+++ src/components/EmployeeList.vue
@@ -0,0 +1,56 @@
+<template>
+
+    <div class="employees-list" v-for="(employee, index) in employees">
+        <div class="employee">
+        <span>Порядковый номер: {{index + 1}}</span> <br>
+        <span>ФИО: {{employee.fullName}}</span> <br>
+        <span>Дата выдачи зарплаты: {{employee.date}}</span> <br>
+        <span>Количество отработанных дней: {{employee.daysWork}}</span> <br>
+        <span>Сумма заработной платы сотрудника без налоговых отчислений: {{employee.salary}}₸</span> <br>
+        <span>Сумма заработной платы сотрудника с налоговыми отчислениями: {{Math.trunc(employee.salary - (employee.salary * 0.15))}}₸</span> <br>
+            <div class="remove">
+                <button type="button" name="plus" class="btn btn-delete" @click="removeEmployee(index)">Удалить</button>
+            </div>
+        </div>
+       </div> 
+       
+</template>
+<script>
+export default {
+    props: {
+        employees: {
+            type: Array,
+            required: true
+        }
+    },
+    methods: {
+        removeEmployee (index) {
+            this.$emit("remove", this.employees, index)
+        }
+    }
+}
+</script>
+<style scoped>
+	.employees-list{
+    margin: 20px 0;
+    padding: 10px;
+    border: 2px solid black;
+    border-radius: 10px;text-align:center;                                                                                                                                
+	}
+
+.btn-delete{
+  width: 100%;
+  background-color: #c52121;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+.btn-delete:hover{
+  background-color: #8b1d1d;
+}
+</style>
+
+
---
+++ src/components/EmployeeResult.vue
@@ -0,0 +1,49 @@
+<template>
+
+    <div class="total-salary">
+        <div class="alert alert-primary" role="alert">
+          Общая сумма заработных плат сотрудников без налоговых: <strong>{{totalSalary}}₸</strong>
+        </div>
+    </div>
+
+    <div class="total-salaryWithNalog">
+        <div class="alert alert-primary" role="alert">
+          Общая сумма заработных плат сотрудников с налогами: <strong>{{totalSalarytax}}₸</strong>
+        </div>
+    </div>
+
+</template>
+<script>
+export default {
+    props: {
+        employees: {
+            type: Array,
+            required: true
+        }
+    },    
+    data() {
+        return {
+            totalSalary: 0,
+            totalSalarytax: 0
+        }
+    },
+    methods: {
+        totalPrice () {
+            this.totalSalary = 0;
+            this.totalSalarytax = 0;
+            for (let i in this.employees) {
+            let salarys = this.employees[i].salary
+            this.totalSalary += salarys
+            this.totalSalarytax += salarys - (salarys * 0.15)
+            }
+        }
+    },
+    mounted() {
+        this.totalPrice()
+    },
+}
+</script>
+<style scoped>
+    
+</style>
+
